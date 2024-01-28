e77ce6878fbb66a3e81e79649637e7de900661cb
興怡 <>
2024-01-27 21:39:58 -0800

    handlers

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 
-export const { handlers, auth } = NextAuth({
+export const { handlers: { GET, POST }, auth } = NextAuth({
   session: { strategy: "jwt" },
   ...authConfig,
 })
