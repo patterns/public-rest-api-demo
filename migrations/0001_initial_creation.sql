a13758805c67bab0be96db790e4c5600e8efa068
興怡 <>
2025-04-04 19:24:31 -0700

    json columns (first migration)

---
+++ migrations/0001_initial_creation.sql
@@ -0,0 +1,18 @@
+-- Migration number: 0001 	 2025-04-05T02:03:21.114Z
+
+
+CREATE TABLE roles (
+  raw_data TEXT
+);
+
+CREATE TABLE users (
+  raw_data TEXT
+);
+
+CREATE TABLE lessons (
+  raw_data TEXT
+);
+
+CREATE TABLE courses (
+  raw_data TEXT
+);
