79f3cdd9de1512f19dbf8936fce54025e8b187a7
興怡 <>
2025-04-06 17:02:22 -0700

    mini seed data

--- migrations/0001_initial_creation.sql
+++ migrations/0001_initial_creation.sql
@@ -2,17 +2,26 @@
 
 
 CREATE TABLE roles (
-  raw_data TEXT
+    -- Access control roles for administration activities.
+    raw_data TEXT
 );
 
 CREATE TABLE users (
-  raw_data TEXT
+    raw_data TEXT
+    role_name AS (json_extract(raw_data, '$.role.name')) STORED
 );
 
 CREATE TABLE lessons (
-  raw_data TEXT
+    raw_data TEXT
 );
 
 CREATE TABLE courses (
-  raw_data TEXT
+    raw_data TEXT
 );
+
+/* --seed data for tests
+INSERT INTO users (raw_data)
+VALUES 
+('{"email": "example@davincirenaissance.org", "providerId": "placeholder", "user_uuid": "ABCDEF123456789", "role": {name: "instructor"}}'),
+('{"email": "learner@davincirenaissance.org", "providerId": "placeholder", "user_uuid": "XYZABC789012345", "role": {name: "learner"}}');
+*/
