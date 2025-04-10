187ca84833f4a4163f45e5670af84ffd6551dee1
興怡 <>
2025-04-10 14:42:05 -0700

    create override

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -24,21 +24,48 @@ const userMeta = {
 export class GetUser extends D1ReadEndpoint { _meta = userMeta; dbName = "DB"; }
 export class ListUsers extends D1ListEndpoint { _meta = userMeta; dbName = "DB"; }
 
+// with create, we want to accept free form JSON for now (require "email" as only rule)
+const CreateModel = z.object({
+    email: z.string().email(),
+}).catchall(z.unknown());
 const createMeta = {
     model: {
-        schema: z.object(),
-        tableName: 'users', // Table name in D1 database
+        schema: CreateModel,
+        tableName: 'users',
     },
 };
 export class CreateUser extends D1CreateEndpoint {
     _meta = createMeta;
     dbName = "DB";
 
-    async create(data: z.object()) {
-        // save 'data' to a database here
+    async create(data: z.infer<typeof CreateModel>) {
+        let inserted;
+        try {
+          const result = await this.getDBBinding()
+            .prepare(
+              `INSERT INTO ${this.meta.model.tableName} (rawdata) VALUES (?) RETURNING *`,
+            )
+            .bind(try {
+                JSON.stringify(data)
+              } catch (e: any) {
+                // capture exception when stringify encounters BigInt/circular
+                JSON.stringify(e, Object.getOwnPropertyNames(e))
+              }
+            )
+            .all();
 
-        return data; // Return the created object
+          inserted = result.results[0] as O<typeof this.meta>;
+        } catch (e: any) {
+          if (e.message.includes("UNIQUE constraint failed")) {
+            const constraintMessage = e.message.split("UNIQUE constraint failed:")[1].split(":")[0].trim();
+            if (this.constraintsMessages[constraintMessage]) {
+              throw this.constraintsMessages[constraintMessage];
+            }
+          }
+
+          throw new ApiException(e.message);
+        }
+        return inserted;
     }
-
 }
 
