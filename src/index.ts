978bd37e7a3d02c9d12dd1d074b0f3a6808d17f6
興怡 <>
2025-04-10 15:36:44 -0700

    lessons endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+
+//import { TaskDelete } from "./endpoints/taskDelete";
+//import { TaskFetch } from "./endpoints/taskFetch";
 import { CreateUser, GetUser, ListUsers } from "./endpoints/users";
+import { CreateLesson, GetLesson, ListLessons } from "./endpoints/lessons";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: { DB: D1Database }}>();
@@ -15,13 +15,14 @@ const openapi = fromHono(app, {
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+//openapi.get("/api/tasks/:taskSlug", TaskFetch);
+//openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 openapi.get("/api/users", ListUsers);
 openapi.post("/api/users", CreateUser);
 openapi.get("/api/users/:id", GetUser);
+openapi.get("/api/lessons", ListLessons);
+openapi.post("/api/lessons", CreateLesson);
+openapi.get("/api/lessons/:id", GetLesson);
 
 // Export the Hono app
 export default app;
